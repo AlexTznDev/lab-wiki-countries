7d5e2dae6cfadacc2a8c36b3195b38d906648b95
AlexTznDev <alex.tuysuzian91@gmail.com>
2023-03-05 23:23:19 +0100

    solved problem on refresh detail page

--- src/component/CountryDetails.jsx
+++ src/component/CountryDetails.jsx
@@ -3,11 +3,14 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function CountryDetails(props) {
-  const [coutryToDisplay, setCoutryToDisplay] = useState();
-  const [allLinkOnBorder, setallLinkOnBorder] = useState();
+  const [coutryToDisplay, setCoutryToDisplay] = useState(null);
+  const [allLinkOnBorder, setallLinkOnBorder] = useState(null);
 
   const params = useParams();
 
+
+
+  
   useEffect(() => {
     if (props.allCountries !== undefined) {
       const filteredCountry = props.allCountries.filter((eachCountry) => {
@@ -18,8 +21,7 @@ function CountryDetails(props) {
         }
       });
 
-      console.log(filteredCountry[0].borders);
-      console.log(props.allCountries);
+
 
       const allCountryLinkFitred = props.allCountries.filter((eachCountry) => {
         if (filteredCountry[0].borders.includes(eachCountry.alpha3Code)) {
@@ -32,11 +34,13 @@ function CountryDetails(props) {
     }
   }, [params.id, props.allCountries]);
 
-  console.log(coutryToDisplay);
+
+
 
   return (
+
     <div style={{ position: 'fixed', top: '28%', right: '30%' }}>
-      {coutryToDisplay !== undefined ? (
+      {coutryToDisplay !== null && coutryToDisplay.length > 0 ? (
         <div>
           <img
             src={`https://flagpedia.net/data/flags/icon/72x54/${coutryToDisplay[0].alpha2Code.toLowerCase()}.png`}
@@ -48,8 +52,8 @@ function CountryDetails(props) {
           <h3>Capital: {coutryToDisplay[0].capital}</h3>
           <h3>Area: {coutryToDisplay[0].area} km2 </h3>
           <h3>
-            Border:
-            {allLinkOnBorder.map((eachCountry) => {
+            Border:{allLinkOnBorder !== null? (
+              allLinkOnBorder.map((eachCountry) => {
               return (
                 <li key={eachCountry.name.common}>
                   <Link
@@ -60,7 +64,9 @@ function CountryDetails(props) {
                   </Link>
                 </li>
               );
-            })}
+            })
+            ):(null)}
+
           </h3>
         </div>
       ) : null}
